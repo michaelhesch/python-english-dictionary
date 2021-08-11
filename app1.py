e5c7b30117b0ef544c2c6c4edeae23774f6da148
michaelhesch <michael.hesch@gmail.com>
2021-08-11 21:27:12 +0100

    expand logic to check for uppercase or acronyms

--- app1.py
+++ app1.py
@@ -12,6 +12,10 @@ def lookup(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         yesno = input(f"Did you mean {get_close_matches(word, data.keys())[0]} instead? Enter Y or N.").lower()
         if yesno == "y":
